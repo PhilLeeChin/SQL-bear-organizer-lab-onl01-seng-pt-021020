313bb74f4a57be5981cc427ee44f0ca8e7bcd66f
Philip Lee-Chin <phil.leechin@outlook.com>
2020-04-12 16:34:58 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,8 +1,9 @@
 CREATE TABLE bears
-  (name TEXT,
-  age INTEGER,
-  gender TEXT,
-  color TEXT,
-  temperament TEXT,
-  alive TEXT
+  (id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    gender TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive TEXT
   );
